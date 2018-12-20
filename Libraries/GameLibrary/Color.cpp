c337f924b5953fa4409e52a6dfdbd2d32b67541a
YdeaGames <ydeagames@gmail.com>
2018-12-20 10:14:59 +0900

    Color Class

--- Libraries/GameLibrary/Color.cpp
+++ Libraries/GameLibrary/Color.cpp
@@ -1,46 +1,80 @@
 #include "Color.h"
 
-// <色コード(標準カラー16色)>
-namespace Colors
+RGBColor::RGBColor(const HSVColor & color)
+	: RGBColor(color.ToRGB())
+{}
+
+HSVColor RGBColor::ToHSV() const
 {
-	// H : 色相　有効範囲 0.0f ～ 360.0f
-	// S : 彩度　有効範囲 0.0f ～ 1.0f
-	// V : 明度　有効範囲 0.0f ～ 1.0f
-	Color GetColorHSV(float H, float S, float V)
+	float h, s, v;
+
+	//最大値・最小値の設定
+	int MAX = std::max((std::max(r, g)), b);
+	int MIN = std::min((std::min(r, g)), b);
+
+	//明度の計算
+	v = MAX / 256.f;
+
+	if (MAX == MIN)
 	{
-		int hi;
-		float f, p, q, t;
-		float r, g, b;
-		int ir, ig, ib;
-
-		hi = (int)(H / 60.0f);
-		hi = hi == 6 ? 5 : hi %= 6;
-		f = H / 60.0f - static_cast<float>(hi);
-		p = V * (1.0f - S);
-		q = V * (1.0f - f * S);
-		t = V * (1.0f - (1.0f - f) * S);
-		switch (hi)
-		{
-		case 0: r = V; g = t; b = p; break;
-		case 1: r = q; g = V; b = p; break;
-		case 2: r = p; g = V; b = t; break;
-		case 3: r = p; g = q; b = V; break;
-		case 4: r = t; g = p; b = V; break;
-		case 5: r = V; g = p; b = q; break;
-		}
-
-		ir = (int)(r * 255.0f);
-		if (ir > 255) ir = 255;
-		else if (ir < 0) ir = 0;
-
-		ig = (int)(g * 255.0f);
-		if (ig > 255) ig = 255;
-		else if (ig < 0) ig = 0;
-
-		ib = (int)(b * 255.0f);
-		if (ib > 255) ib = 255;
-		else if (ib < 0) ib = 0;
-
-		return GetColor(ir, ig, ib);
+		h = 0;
+		s = 0;
 	}
+	else
+	{
+		//色相の計算
+		if (MAX == r) h = 60.f*(g - b) / (MAX - MIN) + 0.f;
+		else if (MAX == g) h = 60.f*(b - r) / (MAX - MIN) + 120.f;
+		else if (MAX == b) h = 60.f*(r - g) / (MAX - MIN) + 240.f;
+
+		if (h > 360.f) h = h - 360.f;
+		else if (h < 0.f) h = h + 360.f;
+
+		//彩度の計算
+		s = static_cast<float>(MAX - MIN) / MAX;
+	}
+
+	return { h, s, v };
+}
+
+HSVColor::HSVColor(const RGBColor & color)
+	: HSVColor(color.ToHSV())
+{}
+
+RGBColor HSVColor::ToRGB() const
+{
+	int hi;
+	float f, p, q, t;
+	float r, g, b;
+	int ir, ig, ib;
+
+	hi = (int)(h / 60.0f);
+	hi = hi == 6 ? 5 : hi %= 6;
+	f = h / 60.0f - static_cast<float>(hi);
+	p = v * (1.0f - s);
+	q = v * (1.0f - f * s);
+	t = v * (1.0f - (1.0f - f) * s);
+	switch (hi)
+	{
+	case 0: r = v; g = t; b = p; break;
+	case 1: r = q; g = v; b = p; break;
+	case 2: r = p; g = v; b = t; break;
+	case 3: r = p; g = q; b = v; break;
+	case 4: r = t; g = p; b = v; break;
+	case 5: r = v; g = p; b = q; break;
+	}
+
+	ir = (int)(r * 255.0f);
+	if (ir > 255) ir = 255;
+	else if (ir < 0) ir = 0;
+
+	ig = (int)(g * 255.0f);
+	if (ig > 255) ig = 255;
+	else if (ig < 0) ig = 0;
+
+	ib = (int)(b * 255.0f);
+	if (ib > 255) ib = 255;
+	else if (ib < 0) ib = 0;
+
+	return { ir, ig, ib };
 }
--- Libraries/GameLibrary/Color.h
+++ Libraries/GameLibrary/Color.h
@@ -1,13 +1,77 @@
 #pragma once
 
+/*
+	// <使用方法>
+
+	// Color、AColor
+	// AColor定数→Color
+	Color color = Colors::Blue & Colors::ColorMask;
+	// Color→AColor
+	AColor acolor = color | Colors::AlphaMask;
+
+	// RGBColor、RGBAColor
+	// AColor定数→RGBColor
+	RGBColor rgb = Colors::Black;
+	// RGBColor→RGBAColor
+	RGBAColor rgba = rgb;
+	// AColor→RGBAColor
+	RGBAColor rgba_1 = acolor;
+	// Color→RGBAColor
+	RGBAColor rgba_2 = color | Colors::AlphaMask;
+	// RGBAColor→RGBColor
+	RGBColor rgb_1 = rgba;
+	// RGBColor→RGBAColor
+	RGBAColor rgba_3 = color;
+	// RGBAColorのRed(int)
+	rgba.r;
+	// RGBAColorのAlpha(float)
+	rgba.A();
+
+	// HSV
+	// RGBColor→HSVColor
+	HSVColor hsv = rgb;
+	// HSVColor→RGBColor
+	RGBColor rgba_4 = hsv;
+	// Color→RGBColor→HSVColor
+	HSVColor hsv_1 = color;
+	// HSVColor→RGBColor→RGBAColor
+	RGBAColor rgba_5 = hsv;
+	// HSVColorのHue(float)
+	hsv.h;
+	// HSVColorのBlue(int)
+	static_cast<RGBColor>(hsv).b;
+	// RGBColorのSaturation
+	static_cast<HSVColor>(rgb).h;
+
+	// Colorへ変換
+	// RGBColor→Color
+	Color color_1 = rgb;
+	// RGBColor→AColor
+	AColor acolor_1 = rgb;
+	// RGBAColor→AColor
+	AColor acolor_2 = rgba;
+	// HSVColor→RGBColor→Color
+	Color color_2 = static_cast<RGBColor>(hsv);
+
+	// 関数
+	auto f = [](RGBAColor color) {};
+	// RGBColor→RGBAColor
+	f(rgb);
+	// AColor定数→RGBAColor
+	f(Colors::Gray);
+	// HSV→RGBAColor
+	f(static_cast<RGBAColor>(hsv));
+*/
+
 // <色コード>
-using Color = unsigned int;
-using AColor = unsigned int;
+using Color = unsigned int;		// アルファなし色
+using AColor = unsigned int;	// アルファ付き色
 
 // <色コード(標準カラー16色)>
 namespace Colors
 {
-	enum : Color
+	// AColor定数
+	enum : AColor
 	{
 		Black = 0xff000000,		// 黒色
 		Navy = 0xff000080,		// 濃紺色
@@ -27,26 +91,37 @@ namespace Colors
 		White = 0xffffffff,		// 白色
 	};
 
-	// H : 色相　有効範囲 0.0f ～ 360.0f
-	// S : 彩度　有効範囲 0.0f ～ 1.0f
-	// V : 明度　有効範囲 0.0f ～ 1.0f
-	Color GetColorHSV(float H, float S, float V);
+	// Color→AColor変換用 (or)
+	const AColor AlphaMask = 0xff000000;
+	// AColor→Color変換用 (and)
+	const AColor ColorMask = 0x00ffffff;
+	inline AColor GetAlphaMask(int alpha) { return (alpha & 0xff) << 24; }
+	inline AColor GetAlphaColor(Color color, int alpha = 255) { return (color & ColorMask) | GetAlphaMask(alpha); }
+	inline AColor GetAlphaMask(float alpha) { return GetAlphaMask(static_cast<int>(alpha * 255.f)); }
+	inline AColor GetAlphaColor(Color color, float alpha) { return GetAlphaColor(color, static_cast<int>(alpha * 255.f)); }
 }
 
+class HSVColor;
+class RGBColor;
+
+// アルファなし色 (0 ～ 255)
 class RGBColor
 {
 public:
 	int r, g, b;
 
 public:
-	RGBColor(int r = 0, int g = 0, int b = 0)
+	RGBColor(int r, int g, int b)
 		: r(r)
 		, g(g)
 		, b(b) {}
-	RGBColor(Color color = 0x000000)
+	RGBColor()
+		: RGBColor(0, 0, 0) {}
+	RGBColor(Color color)
 		: RGBColor(static_cast<int>((color >> 16) & 0xff), static_cast<int>((color >> 8) & 0xff), static_cast<int>((color >> 0) & 0xff)) {}
-	RGBColor(float R = 0, float G = 0, float B = 0)
+	RGBColor(float R, float G, float B)
 		: RGBColor(static_cast<int>(R * 255.f), static_cast<int>(G * 255.f), static_cast<int>(B * 255.f)) {}
+	RGBColor(const HSVColor& color);
 
 public:
 	inline float R() { return r / 255.f; } const
@@ -55,41 +130,58 @@ public:
 	inline void SetR(float R) { r = static_cast<int>(R * 255.f); }
 	inline void SetG(float G) { g = static_cast<int>(G * 255.f); }
 	inline void SetB(float B) { b = static_cast<int>(B * 255.f); }
-	inline AColor GetColor() { return ((r & 0xff) << 16) | ((g & 0xff) << 8) | ((b & 0xff) << 0); }
+	inline Color GetColor() { return (((r & 0xff) << 16) | ((g & 0xff) << 8) | ((b & 0xff) << 0)) | Colors::AlphaMask; }
+	inline operator Color() { return GetColor(); }
 
 public:
-
-
+	HSVColor ToHSV() const;
 };
 
+// アルファ付き色 (0 ～ 255)
+// Colorからの変換時はColors::AlphaMaskを使用してください
 class RGBAColor : public RGBColor
 {
 public:
 	int a;
 
 public:
-	RGBAColor(int r = 0, int g = 0, int b = 0, int a = 0)
+	RGBAColor(int r, int g, int b, int a = 255)
 		: RGBColor(r, g, b)
 		, a(a) {}
-	RGBAColor(AColor color = 0xff000000)
+	RGBAColor(const RGBColor& color)
+		: RGBColor(color)
+		, a(255) {}
+	RGBAColor()
+		: RGBAColor(0, 0, 0, 255) {}
+	RGBAColor(AColor color)
 		: RGBAColor(static_cast<int>((color >> 16) & 0xff), static_cast<int>((color >> 8) & 0xff), static_cast<int>((color >> 0) & 0xff), static_cast<int>((color >> 24) & 0xff)) {}
-	RGBAColor(float R = 0, float G = 0, float B = 0, float A = 0)
+	RGBAColor(float R, float G, float B, float A = 1)
 		: RGBAColor(static_cast<int>(R * 255.f), static_cast<int>(G * 255.f), static_cast<int>(B * 255.f), static_cast<int>(A * 255.f)) {}
 
 public:
 	inline float A() { return a / 255.f; } const
 	inline void SetA(float A) { a = static_cast<int>(A * 255.f); }
 	inline AColor GetColor() { return ((a & 0xff) << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | ((b & 0xff) << 0); }
+	inline operator AColor() { return GetColor(); }
 };
 
+// H : 色相　有効範囲 0.0f ～ 360.0f
+// S : 彩度　有効範囲 0.0f ～ 1.0f
+// V : 明度　有効範囲 0.0f ～ 1.0f
 class HSVColor
 {
 public:
 	float h, s, v;
 
 public:
-	HSVColor(float h = 0, float s = 0, float v = 0)
+	HSVColor(float h, float s, float v)
 		: h(h)
 		, s(s)
 		, v(v) {}
+	HSVColor()
+		: HSVColor(0, 0, 0) {}
+	HSVColor(const RGBColor& color);
+
+public:
+	RGBColor ToRGB() const;
 };
