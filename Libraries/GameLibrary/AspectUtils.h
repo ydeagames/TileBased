7ca99424cff2bd6f38daaa51366af22ae910e5d1
YdeaGames <ydeagames@gmail.com>
2019-01-21 05:49:08 +0900

    AspectUtils

---
+++ Libraries/GameLibrary/AspectUtils.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace AspectUtils
+{
+	Vector2 Raw(Vector2 size, Vector2 maxsize);
+	Vector2 Max(Vector2 size, Vector2 maxsize);
+	Vector2 Width(Vector2 size, Vector2 maxsize);
+	Vector2 Height(Vector2 size, Vector2 maxsize);
+	Vector2 Inner(Vector2 size, Vector2 maxsize);
+	Vector2 Outer(Vector2 size, Vector2 maxsize);
+	Vector2 WidthLimit(Vector2 size, Vector2 maxsize);
+	Vector2 HeightLimit(Vector2 size, Vector2 maxsize);
+	Vector2 Limit(Vector2 size, Vector2 maxsize);
+}
